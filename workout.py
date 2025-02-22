2a0430731ba139c39972c053412838867e00b6ff
gettingfunkier <dev.gettingfunkier@gmail.com>
2025-02-22 16:33:53 +0000

    Add files via upload

--- workout.py
+++ workout.py
@@ -1,37 +1,25 @@
-workout = {}
+import json
 
-def action():
-    while True:
-        act = input("a - add exercise\ni - import data\nv - view workout\ne - export\nr - reset\nq - quit\n")
-        if act == "a":
-            addEx()
-        if act == "i":
-            importData(workout)
-        if act == "v":
-            viewWorkout(workout)
-        if act == "e":
-            exportFile(workout)
-        if act == "r":
-            resetData(workout)
-        if act == "q":
-            print("Godspeed!")
-            break
+filename = "workout.json"
+def importData(): # gets information from JSON file
+    workout = {}
+    with open(filename, "r") as file:
+        return json.load(file)
 
-def addEx():
-    entry = input("Entere exercise (name,sets,reps,weight):")
-    info = entry.split(",")
+def saveFile(workout): # saves information to JSON file
+    with open(filename, "w") as file:
+        json.dump(workout, file, indent = 4)
 
-    name = str(info[0])
-    sets = str(info[1])
-    reps = str(info[2])
-    weight = str(info[3])
-
+def addEx(workout): # writes a new exercise
+    name = input("name: ")
+    sets = input("sets: ")
+    reps = input("reps: ")
+    weight = input("weight: ")
     workout[name] = {'sets': sets, 'reps': reps, 'weight': weight}
     print(f"'{name}' logged!")
+    saveFile(workout)
 
-    return workout
-
-def viewWorkout(workout):
+def viewWorkout(): # prints current workout
     if not workout:
         print("No exercises logged yet!")
         return
@@ -42,39 +30,43 @@ def viewWorkout(workout):
         weight = info['weight']
         print(f"{name}: {sets} sets of {reps} reps, at {weight}kg")
 
-def importData(workout):
-    confirm = input("will override current workout, proceed? y/n\n")
-    
-    if confirm == "y":
-        file = open("workout.txt")
-
-        for line in file:
-            split = line.strip().split(',')
-            name = split[0]
-            sets = split[1]
-            reps = split[2]
-            weight = split[3]
-
-            workout[name] = {'sets': sets, 'reps': reps, 'weight': weight}
-
-        print("data logged!")
-
-def exportFile(workout):
-    with open('workout.txt', 'w') as f:
+def exportFile(workout): # creates a txt file with workout info
+    workout = importData()
+    with open("workout.txt", "w") as file:
+        heading = input("give your workout a title: ")
+        weekday = input(f"weekdays for {heading}: ")
+        file.write(f"{heading}\nTo practice on {weekday}:\n")
         for name, info in workout.items():
             sets = info['sets']
             reps = info['reps']
             weight = info['weight']
+            file.write(f"{name}: {sets} sets of {reps} reps, @ {weight}kg\n")
+        print("exported successfully!")
             
-            f.write(f"{name},{sets},{reps},{weight}\n")
-        print("data exported!")
-    
-def resetData(workout):
+def resetData(workout): # erases all data
     confirm = input("will erase all data, proceed? y/n\n")
 
     if confirm == "y":
         workout.clear()
+        with open(filename, "w") as file:
+            json.dump(workout, file,)
+        
+        with open("workout.txt", "r+") as file:
+            for line in file:
+                file.write("")
 
-    return workout
 
-action()
+while True:
+    act = input("(a) - add exercise (v) - view workout (e) - export (r) - reset (q) - quit\n")
+    workout = importData()
+    if act == "a":
+        addEx(workout)
+    if act == "v":
+        viewWorkout()
+    if act == "e":
+        exportFile(workout)
+    if act == "r":
+        resetData(workout)
+    if act == "q":
+        print("Godspeed!")
+        break
